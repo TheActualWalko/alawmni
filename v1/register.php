fea6e3e5239025052bfc1edc8256fe846c37551f
Sam Watkinson <sam.ws.watkinson@gmail.com>
2017-07-28 09:11:42 -0400

    whoops

--- v1/register.php
+++ v1/register.php
@@ -1,5 +1,5 @@
 <?php
-  $mysqli = new mysqli('127.0.0.1', 'root', 'EbFahux8', 'kcl_students');
+  $mysqli = new mysqli('127.0.0.1', 'root', $MYSQL_PWD, 'kcl_students');
   $mysqli->set_charset("utf8");
   if ($mysqli->connect_errno) {
     echo "Sorry, this website is experiencing problems.";
