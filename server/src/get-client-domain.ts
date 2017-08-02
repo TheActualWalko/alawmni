5fa4e33fe4efdb2015165162b673f0c701b86e7e
Sam Watkinson <sam.ws.watkinson@gmail.com>
2017-08-02 18:46:58 -0400

    client defaults

--- server/src/get-client-domain.ts
+++ server/src/get-client-domain.ts
@@ -1,6 +1,11 @@
 export default (req, res, next) => {
   if (req.headers['host'].includes('localhost') || req.headers['host'].includes('51.255.193.170')) {
-    req.clientDomain = req.cookies['domain'];
+    const cookie = req.cookies['domain'];;
+    if (cookie) {
+      req.clientDomain = cookie;
+    } else {
+      req.clientDomain = 'test';
+    }
   } else {
     req.clientDomain = req.headers['host'].split(':')[0];
   }
