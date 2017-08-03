5b1849a37df834d74ff856aea3ad77390d12773e
Sam Watkinson <sam.ws.watkinson@gmail.com>
2017-08-03 09:24:51 -0400

    dropdown styling

--- server/public/src/autocomplete-input.tsx
+++ server/public/src/autocomplete-input.tsx
@@ -6,6 +6,6 @@ const noop = (x?) => {}
 export default ({id, label, data, onSelect = noop, onChange = noop})=>{
   return <div className="input autocomplete-input">
     <label htmlFor={id}>{label}</label>
-    <DropdownInput data={data} onChange={onChange} onSelect={onSelect} suggest={true} filter="contains" placeholder="Type here" />
+    <DropdownInput data={data} onChange={onChange} onSelect={onSelect} suggest={true} filter="contains" placeholder="Type here" duration={0} />
   </div>
 };
