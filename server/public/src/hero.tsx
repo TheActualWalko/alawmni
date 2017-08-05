7ebb94e79a757e364b4ab83bb81c5f533965fb11
Sam Watkinson <sam.ws.watkinson@gmail.com>
2017-08-05 11:16:08 -0400

    make h1 a link

--- server/public/src/hero.tsx
+++ server/public/src/hero.tsx
@@ -1,11 +1,14 @@
 import React = require('react');
 import {connect} from 'react-redux';
+import {push} from 'react-router-redux';
 
-const Hero = ({clientWebsite}) => (
+const Hero = ({clientWebsite, push}) => (
   <header className="main-header">
     <h1>
-      <span>Alumni</span><br />
-      <span>Database</span>
+      <a onClick={()=>push('/')}>
+        <span>Alumni</span><br />
+        <span>Database</span>
+      </a>
     </h1>
     <h2><a href={clientWebsite} title="Home" rel="noopener"><img src="img/logo.png" alt="Logo" /></a></h2>
   </header>
@@ -15,4 +18,6 @@ const mapStateToProps = (state) => ({
   clientWebsite: state.statics.clientWebsite
 });
 
-export default connect(mapStateToProps)(Hero);
+const mapDispatchToProps = {push}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Hero);
